e6ada9e2875078e70e96ef9735445c6484944e3f
AlexanderN <AlexanderN@Mac-mini-Aleksandr-2.local>
2017-09-21 15:34:47 +0300

    Add some assets And create categories view controller with table view in it

---
+++ coder-swag/View/CategoryTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryTableViewCell.swift
+//  coder-swag
+//
+//  Created by AlexanderN on 21.09.17.
+//  Copyright © 2017 AlexanderN. All rights reserved.
+//
+
+import UIKit
+
+class CategoryTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
